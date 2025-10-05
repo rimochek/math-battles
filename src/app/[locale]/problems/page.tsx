a0712b7f885f46c0c833dae7dc758e6612a286fd
Trenod <126702826+Tren0d@users.noreply.github.com>
2025-10-05 22:46:08 +0500

    Update page.tsx

--- src/app/[locale]/problems/page.tsx
+++ src/app/[locale]/problems/page.tsx
@@ -1,26 +1,32 @@
-"use client"
+import { useTranslations } from "next-intl";
+import { FileText } from "lucide-react";
 
-import { useTranslations } from "next-intl"
-
-export const dynamic = "force-dynamic" // Forces the page to be rendered dynamically
-
-export default function NotFoundPage() {
-  const t = useTranslations("problems")
+export default function PastProblems() {
+  const t = useTranslations("problems");
+  const t_files = useTranslations();
+  const problems = t_files.raw("problems.files");
 
   return (
-    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-      <div className="mx-auto max-w-screen-sm text-center">
-        <h1
-          className="mb-4 text-3xl tracking-tight font-extrabold lg:text-7xl text-primary-600 dark:text-primary-500"
-          style={{ color: "#1D3AAC" }}
-        >
-          {t("inDevelopment")}
-        </h1>
-        <p className="mb-4 text-xl tracking-tight font-bold text-gray-900 md:text-3xl">
-          {t("notReady")}
-        </p>
-        <p className="mb-4 text-lg font-light text-black">{t("iSwear")}</p>
+    <div className="mx-auto max-w-4xl p-6 lg:px-8 py-12">
+      <div className="flex flex-col justify-center items-center mb-8">
+        <h2 className="text-4xl font-bold text-[#1D3AAC] mb-2">{t("title")}</h2>
+        <div className="h-1 w-60 rounded-lg" style={{ backgroundColor: "#1D3AAC" }} />
       </div>
+      <ul className="space-y-3 max-w-2xl mx-auto">
+        {problems.map((item, index) => (
+          <li key={index}>
+            <a
+              href={item.file}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2 text-2xl text-blue-700 hover:text-blue-900 transition"
+            >
+              <FileText className="w-5 h-5 text-[#1D3AAC]" />
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
