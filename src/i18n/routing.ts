5a2126a468d9bc8833bf6772cd4a144f234d615c
Trenod <126702826+Tren0d@users.noreply.github.com>
2025-10-05 15:30:59 +0500

    Update routing.ts
    kz

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,25 +2,29 @@ import { createNavigation } from "next-intl/navigation"
 import { defineRouting } from "next-intl/routing"
 
 export const routing = defineRouting({
-  locales: ["en", "ru"],
+  locales: ["en", "ru", "kz"],
   defaultLocale: "ru",
   pathnames: {
     "/": "/",
     "/our-team": {
       en: "/our-team",
       ru: "/наша-команда",
+      kz: "/біздің-команда",
     },
     "/problems": {
       en: "/problems",
       ru: "/материалы",
+      kz: "/материалдар",
     },
     "/gallery": {
       en: "/gallery",
       ru: "/галерея",
+      kz: "/галерея",
     },
     "/registration": {
       en: "/registration",
       ru: "/регистрация",
+      kz: "/тіркеу",
     },
   },
 })
