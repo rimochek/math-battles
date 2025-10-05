39a7a02bd996906a1b78a0fc9d6daca7622de16a
Trenod <126702826+Tren0d@users.noreply.github.com>
2025-10-05 22:55:18 +0500

    Update page.tsx

--- src/app/[locale]/problems/page.tsx
+++ src/app/[locale]/problems/page.tsx
@@ -9,7 +9,7 @@ export default function PastProblems() {
   return (
     <div className="mx-auto max-w-4xl p-6 lg:px-8 py-12">
       <div className="flex flex-col justify-center items-center mb-8">
-        <h2 className="text-4xl font-bold text-[#1D3AAC] mb-2">{t("title")}</h2>
+        <h2 className="text-4xl font-bold mb-2">{t("title")}</h2>
         <div className="h-1 w-60 rounded-lg" style={{ backgroundColor: "#1D3AAC" }} />
       </div>
       <ul className="space-y-3 max-w-2xl mx-auto">
