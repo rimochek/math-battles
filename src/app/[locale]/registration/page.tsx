0917a6b8915733115bb9ac4680c98b01d80b9440
Trenod <126702826+Tren0d@users.noreply.github.com>
2025-10-08 11:01:33 +0500

    registration form

--- src/app/[locale]/registration/page.tsx
+++ src/app/[locale]/registration/page.tsx
@@ -9,6 +9,7 @@ export default function RegistrationPage() {
       <div className="mx-auto max-w-4xl items-center justify-between p-6 lg:px-8 pt-12 pb-16">
         <p className="font-bold text-4xl mb-5">{t("registration")}</p>
         <p className="text-xl">{t("registrationContext")}</p>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd-QGpFGTRCJIAeHA8qB8U59wbEGivFqgq1rHeZOXnt0-de3Q/viewform?embedded=true" width="640" height="5917" frameborder="0" marginheight="0" marginwidth="0">Загрузка…</iframe>
       </div>
     </div>
   )
