899f954afb37ef7e4084296761c8124771ab48bb
Trenod <mtimz.1207@gmail.com>
2025-10-08 16:46:32 +0500

    Revert "13 ballov"
    This reverts commit f456288dfa1dd1b386d436290a36f2b02ef0e3e0.

--- src/i18n/navigation.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { createNavigation } from "next-intl/navigation";
-import { routing } from "./routing";
-
-export const { Link, getPathname, redirect, usePathname, useRouter } =
-  createNavigation(routing);
