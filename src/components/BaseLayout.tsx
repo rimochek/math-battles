e6296b62b34757ce0c1a12d59356b60b7d8bfa07
Trenod <126702826+Tren0d@users.noreply.github.com>
2025-10-17 22:17:44 +0500

    google analytics try 1

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -3,6 +3,7 @@ import { getMessages } from "next-intl/server"
 import { ReactNode } from "react"
 import Navigation from "@/components/Navigation"
 import Footer from "./Footer"
+import Script from "next/script"
 
 type Props = {
   children: ReactNode
@@ -14,6 +15,20 @@ export default async function BaseLayout({ children, locale }: Props) {
 
   return (
     <html className="h-full" lang={locale}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZS8CNS2LJZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZS8CNS2LJZ');
+          `}
+        </Script>
+      </head>
       <body className="flex min-h-screen flex-col">
         <NextIntlClientProvider messages={messages}>
           <Navigation />
